77021babc3765cb2519d1aa98be3d502f6cfdff7
Sean McDaniel <seanhb714@gmail.com>
2023-08-20 15:22:16 -0700

    hide env

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import AuthProvider from './AuthProvider'
 import './globals.css'
 import NavMenu from './NavMenu'
 
@@ -13,11 +14,13 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body>
-        <NavMenu />
-        {children}
-        </body>
-    </html>
+    <AuthProvider>
+      <html lang="en">
+        <body>
+          <NavMenu />
+          {children}
+          </body>
+      </html>
+    </AuthProvider>
   )
 }
